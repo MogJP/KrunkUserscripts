c44db2bd0f2829118a4c7e49265f6cf68ff678ef
MogJP <81292406+MogJP@users.noreply.github.com>
2023-02-16 21:57:58 +0900

    Update and rename ChatColorizer to ChatColorizer.user.js

--- ChatColorizer.user.js
+++ ChatColorizer.user.js
@@ -7,8 +7,8 @@
 // @match        https://krunker.io/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=krunker.io
 // @grant        none
-// @updateURL    https://github.com/hepokon365/updatable_userscript/raw/master/src/sample.user.js
-// @downloadURL  https://github.com/hepokon365/updatable_userscript/raw/master/src/sample.user.js
+// @updateURL    https://github.com/MogJP/KrunkUserscripts/edit/main/ChatColorizer.user.js
+// @downloadURL  https://github.com/MogJP/KrunkUserscripts/edit/main/ChatColorizer.user.js
 // ==/UserScript==
 
 var monitorElement = window.document.getElementById('chatList');
